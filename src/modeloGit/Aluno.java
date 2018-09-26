cb01903f64552c22a7b4523be2e1e6997978a66f
Caio <c.cesar.ferreira@uol.com.br>
2018-09-26 19:48:11 -0300

    Adição do Arquivo Gitignore e atualizacao da classe Aluno

--- src/modeloGit/Aluno.java
+++ src/modeloGit/Aluno.java
@@ -1,5 +1,37 @@
 package modeloGit;
 
 public class Aluno {
+	
+	private int matricula;
+	private String nome;
+	private String cpf;
+	private String email;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String email() {
+		return email;
+	}
+	public void setemail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
